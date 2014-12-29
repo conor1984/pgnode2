26ef4449b1f3b0369118ba1162fbc24de5af88d3
conor1984 <conor.nagle1984@gmail.com>
2014-12-29 10:27:56 -0500

    Update failover.sh

--- failover.sh
+++ failover.sh
@@ -1,9 +1,9 @@
 # this script should do the following 3 things, promote db , copy
 # pgbouncer conf to pgbouncer server and restart the service
 pg_ctl -D $PGDATA promote
-rsync --delete -a -k --perms  $PGBOUNCE/pgbouncer.ini maximus@pgbouncer:$PGBOUNCE/
+rsync --delete -a -k --perms  $PGBOUNCE/pgbouncer.ini postgres@pgbouncer:$PGBOUNCE/
 #reload
-ssh maximus@pgbouncer pgbouncer -R -d $PGBOUNCE/pgbouncer.ini
+ssh postgres@pgbouncer pgbouncer -R -d $PGBOUNCE/pgbouncer.ini
 #email
 echo "pgnode 1 has failed" | mail -s "node failure"  conor.nagle@firmex.com
 #tell node3 to follow
